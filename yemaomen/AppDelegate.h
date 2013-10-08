19a32fa35afad9a7e1ec4bf67545c8d95184a0b7
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-08 10:18:22 +0800

    Initial Commit

---
+++ yemaomen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yemaomen
+//
+//  Created by Zhu Weifeng on 10/8/13.
+//  Copyright (c) 2013 yemaomen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ yemaomen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yemaomen
+//
+//  Created by Zhu Weifeng on 10/8/13.
+//  Copyright (c) 2013 yemaomen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
