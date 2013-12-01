caa7cb7ec6ee0210a651605ceaa6b5d98475bada
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-12-01 22:35:28 +0800

    给自定义log添加注释

--- yemaomen/YMMUtilities.h
+++ yemaomen/YMMUtilities.h
@@ -11,6 +11,8 @@
 // 全局宏定义开始
 
 // Custom Logging
+// FINAL_RELEASE是在building settings中添加的宏定义，debug中没有定义这个值，所以会打印，release中定义了这个值，所以不会打印。
+// 任何需要判断是不是release而进行条件编译的代码都可以用这个句式。
 #if defined(FINAL_RELEASE)
   #define YMMLOG(format, ...)
 #else
