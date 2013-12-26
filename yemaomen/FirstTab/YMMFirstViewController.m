a05fc3bd3f269215f80cb69d5bd8416e8f015376
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-12-26 15:26:34 +0800

    测试BaseRefreshViewController完成

--- yemaomen/FirstTab/YMMFirstViewController.m
+++ yemaomen/FirstTab/YMMFirstViewController.m
@@ -24,14 +24,15 @@
   return self;
 }
 							
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
   [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  
+  // 加载最新数据
+  [self loadingLatestData];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
