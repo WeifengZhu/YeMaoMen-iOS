dd8f4c1b10527b7ca12d007f69ea988dfbf5dffb
WeifengZhu <weifeng.zhu.ru@gmail.com>
2014-01-05 22:45:28 +0800

    更新User model

--- yemaomen/Models/YMMUser.h
+++ yemaomen/Models/YMMUser.h
@@ -10,8 +10,16 @@
 
 @interface YMMUser : NSObject
 
-- (id)initWithDictionary:(NSDictionary *)dict;
-
 @property (nonatomic, strong, readonly) NSString *username;
+@property (nonatomic, strong, readonly) NSString *bio;
+@property (nonatomic, strong, readonly) NSString *location;
+@property (nonatomic, strong, readonly) NSNumber *score;
+@property (nonatomic, strong, readonly) NSString *avatarURL;
+@property (nonatomic, strong, readonly) NSNumber *allowBrowse;
+@property (nonatomic, strong, readonly) NSString *identifyToken;
+
++ (YMMUser *)currentUser;
+
+- (id)initWithDictionary:(NSDictionary *)dict;
 
 @end
